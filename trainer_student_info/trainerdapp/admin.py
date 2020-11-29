9f2787afe7773f58a559f758fa0312825bfd8343
Dinesh-Stack97 <68021533+Dinesh-Stack97@users.noreply.github.com>
2020-11-29 19:32:49 +0530

    Add files via upload

---
+++ trainer_student_info/trainerdapp/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from .models import Trainer_Info
+
+# Register your models here.
+class Trainer_InfoAdmin(admin.ModelAdmin):
+	'''
+	Admin view for Trainer Information
+
+	'''
+	list_display = ('FIRSTNAME','LASTNAME','DOMAIN','EXPERIENCE','BRANCH','ADDRESS',
+		'NO_OF_BATCHES','SALARY')
+
+	admin.site.register(Trainer_Info, Trainer_InfoAdmin)
---
+++ trainer_student_info/trainerdapp/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class TrainerdappConfig(AppConfig):
+    name = 'trainerdapp'
